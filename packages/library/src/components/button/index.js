9e8b0b6ef382263f793d85f8f60f29b4c2d7dd4d
kirstie-langan <kirstie.langan@clearScore.com>
2022-06-29 20:10:41 +0100

    Monorepo set-up

---
+++ packages/library/src/components/button/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import MatButton from "@material-ui/core/Button";
+
+// const Button = ({ type, disabled, children }) => {
+//   return (
+//     <MatButton variant="contained" color={type} disabled={disabled}>
+//       {children}
+//     </MatButton>
+//   );
+// };
+
+// export default Button;
