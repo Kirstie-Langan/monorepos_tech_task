382644d08cc91e6a9f881c22863d1acff94327c9
kirstie-langan <kirstie.langan@clearScore.com>
2022-07-01 15:52:26 +0100

    working multirepo

---
+++ packages/library/src/components/Button.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface Props {
+    onClick: () => void
+}
+
+const Button: React.FC<Props> = (props) => {
+
+    return (
+        <button {...props}>Click</button>
+    )
+}
+
+export default Button;
--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "@monorepos-tech-task/components";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  console.log(Button);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        your count is {count}
-        <button onClick={() => setCount((prev) => ++prev)}>Click</button>
+        Your count is {count}
+        <Button onClick={() => setCount((prev) => ++prev)} />
       </header>
     </div>
   );
 }
-
 
 export default App;
